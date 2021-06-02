2c2a70557f69c106cbd765e980cac82a5dafaf9b
marvin <marvinma.mw@gmail.com>
2021-06-02 18:05:58 -0400

    READY TO MERGE

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const taskRouter = express.Router();
 const { asyncHandler, csrfProtection } = require('../utils');
 const { Task } = require('../db/models');
 const cookieParser = require('cookie-parser');
