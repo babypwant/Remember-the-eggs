422cf86832f656346189f04a14a13af85be657f5
AK <austinkuemper@gmail.com>
2021-06-02 09:16:04 -0700

    added boilerplate get route for login page

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const { asyncHandler, csrfProtection } = require('../utils');
+const { User } = require('../db/models');
+const cookieParser = require('cookie-parser');
 
+router.get(
+  '/',
+  csrfProtection,
+  asyncHandler(async (req, res, next) => {
+    // const {username, fullName, } = req.body
+  })
+);
 
-module.exports = router
+module.exports = router;
