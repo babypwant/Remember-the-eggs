060fb177e1da4511362ddf239f7f01f83419b64f
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-06-03 14:09:38 -0700

    Fixed home route

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,22 @@ var express = require('express');
 var router = express.Router();
 const { asyncHandler } = require('../utils.js')
 const { User, List, Task } = require('../db/models')
-const {loginUser, logoutUser, requireAuth, restoreUser} = require('../auth')
+const { loginUser, logoutUser, requireAuth, restoreUser } = require('../auth')
 
 /* GET home page. */
-router.get('/', asyncHandler(async (req, res)=> {
-  const {userId} = req.session.auth
-  console.log("string", req.session.auth)
+router.get('/', asyncHandler(async (req, res) => {
+  if (!req.session.auth) {
+    res.redirect('/login')
+  } else {
+    const { userId } = req.session.auth
+  }
 
 
   const user = await User.findByPk(userId)
-  const lists = await List.findAll({where:{userId}, include:{model: Task}})
+  const lists = await List.findAll({ where: { userId }, include: { model: Task } })
 
 
-  res.render('home', { title: 'a/A Express Skeleton Home', user, lists  });
+  res.render('home', { title: 'a/A Express Skeleton Home', user, lists });
 }));
 
 module.exports = router;
