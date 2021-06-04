9225dd9b4e1eb7fc6bbabdad37fa22ccda4beaa0
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-06-04 14:21:43 -0700

    adding tasks routes and event listeners in index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,79 @@
-
+const taskForm = document.querySelector(".create-task-form");
+taskForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get("name");
+    const due = formData.get("due");
+    const completionStatus = formData.get("completionStatus");
+    const description = formData.get("description");
+    const listId = formData.get("listId");
+    const body = { name, due, completionStatus, description, listId };
+    try {
+        const res = await fetch("http://localhost:8080/lists", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (res.status === 401) {
+            window.location.href = "/log-in";
+            return;
+        }
+        if (!res.ok) {
+            throw res;
+        }
+        form.reset();
+    } catch (err) {
+        handleErrors(err);
+    }
+});
+const updateButton = document.querySelector(".task-update-button");
+updateButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const name = document.querySelector('.new-task-name').value
+    const due = document.querySelector('.new-task-due').value
+    const completionStatus = document.querySelector('.new-task-completionStatus').value
+    const description = document.querySelector('.new-task-description').value
+    const listId = document.querySelector('.new-task-listId').value
+    const body = { name, due, completionStatus, description, listId };
+    try {
+        const res = await fetch(`http://localhost:8080/lists/${e.target.id}`, {
+            method: "PUT",
+            body: JSON.stringify({ body }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+        if (res.status === 401) {
+            window.location.href = "/log-in";
+            return;
+        }
+        if (!res.ok) {
+            throw res;
+        }
+        window.location.href = "/"; s
+    } catch (err) {
+        handleErrors(err);
+    }
+});
+const taskDeleteButton = document.querySelector('.task-delete-button');
+taskDeleteButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+        console.log('Hello from the destroy world!');
+        const res = await fetch(`http://localhost:8080/tasks/${e.target.id}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await res.json();
+    } catch (err) {
+        console.log(err)
+    }
+});
 
 const handleErrors = async (err) => {
     if (err.status >= 400 && err.status < 600) {
@@ -72,7 +147,6 @@ deleteButton.addEventListener('click', async (e) => {
                 "Content-Type": "application/json",
             }
         });
-        //const data = await res.json();
 
     } catch (err) {
         console.log(err)
